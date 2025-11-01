ad2b80e98400389ad6f1e42ad9f8cc177ceda2b9
edicleitonalves-ctrl <edicleiton.costa@gmail.com>
2025-11-01 14:15:45 +0000

    Molhoramos na legibilidade dos printf

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,21 +4,41 @@ int main() {
     // Variáveis do Cartão "Super Trunfo" - Recife/Pernambuco
     char estado[] = "Pernambuco";
     char codigo[20] = "A01";
-    char capital[50] = "Recife"; // Renomeei "Estado" para "capital" para evitar confusão.
+    char capital[50] = "Recife";
     int populacao = 1488920; 
-    float area = 218.8;      
-    float pib = 54.97;       
+    float area = 218.8;        // km²      
+    float pib = 54.97;          // PIB em Bilhões R$       
     int turismo = 20;
 
-    // Imprimindo o valor da string "estado"
-    printf("--- Cartão Super Trunfo ---\n");
+    // Variáveis do Cartão "Super Trunfo" - Maceió/Alagoas
+    char a_estado[] = "Alagoas";
+    char a_codigo[20] = "B01";
+    char a_capital[50] = "Maceió";
+    int a_populacao = 3127511;
+    float a_area = 27843.2;        // km²
+    float a_pib = 63.202;          // PIB em Bilhões R$
+    int a_turismo = 9;
+
+    // Imprimindo o valor da string "Pernambuco"
+    printf("--- Card 1 ---\n");
     printf("Código: %s\n", codigo);
     printf("Estado: %s\n", estado);
     printf("Capital: %s\n", capital);
     printf("População: %d habitantes\n", populacao);
     printf("Área: %.1f km²\n", area); // Use %.1f para formatar float com 1 casa decimal
-    printf("PIB (Bilhões): R$ %.2f\n", pib); // Use %.2f para formatar float com 2 casas decimais
+    printf("PIB: R$ %.2f  (Bilhões)\n", pib); // Use %.2f para formatar float com 2 casas decimais
     printf("Pontos Turísticos: %d\n", turismo);
+    printf("---------------------------\n");
+
+    // Imprimindo o valor da string "Alagoas"
+    printf("--- Card 2 ---\n");
+    printf("Código: %s\n", a_codigo);
+    printf("Estado: %s\n", a_estado);
+    printf("Capital: %s\n", a_capital);
+    printf("População: %d habitantes\n", a_populacao);
+    printf("Área: %.1f km²\n", a_area); // Use %.1f para formatar float com 1 casa decimal
+    printf("PIB: R$ %.2f  (Bilhões)\n", a_pib); // Use %.2f para formatar float com 2 casas decimais
+    printf("Pontos Turísticos: %d\n", a_turismo);
     printf("---------------------------\n");
 
 
