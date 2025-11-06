ad811e149e14363280b9f23c06249c3c5f6432e0
edicleitonalves-ctrl <edicleiton.costa@gmail.com>
2025-11-06 20:23:01 +0000

    Inclusão do cálculo PIB per capito e densidade populacional

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,13 +2,19 @@
 
 int main() {
     // Variáveis do Cartão "Super Trunfo" - Recife/Pernambuco
-    char estado[] = "Pernambuco";
-    char codigo[20] = "A01";
-    char capital[50] = "Recife";
-    int populacao = 1488920; 
-    float area = 218.8;        // km²      
-    float pib = 54.97;          // PIB em Bilhões R$       
-    int turismo = 20;
+    char p_estado[] = "Pernambuco";
+    char p_codigo[20] = "A01";
+    char p_capital[50] = "Recife";
+    int p_populacao = 1488920; 
+    float p_area = 218.8;        // km²      
+    float p_pib = 54.97;          // PIB em Bilhões R$       
+    int p_turismo = 20;
+    // 1. CÁLCULO DA DENSIDADE POPULACIONAL (população / área)
+    // O resultado deve ser float para precisão.
+    float p_densidade_pop = (float)p_populacao / p_area;
+    // 2. CÁLCULO DO PIB PER CAPITA ((PIB em Bilhões * 1.000.000.000) / população)
+    float p_pib_reais = p_pib * 1000000000.0;
+    float p_pib_per_capita = p_pib_reais / (float)p_populacao;
 
     // Variáveis do Cartão "Super Trunfo" - Maceió/Alagoas
     char a_estado[] = "Alagoas";
@@ -18,16 +24,26 @@ int main() {
     float a_area = 27843.2;        // km²
     float a_pib = 63.202;          // PIB em Bilhões R$
     int a_turismo = 9;
+    // 1. CÁLCULO DA DENSIDADE POPULACIONAL (população / área)
+    float a_densidade_pop = (float)a_populacao / a_area;
+    // 2. CÁLCULO DO PIB PER CAPITA ((PIB em Bilhões * 1.000.000.000) / população)
+    float a_pib_reais = a_pib * 1000000000.0;
+    float a_pib_per_capita = a_pib_reais / (float)a_populacao;
 
+    //título do jogo ( Super Trunfo)
+    printf("*** Super Trunfo ***\n");
     // Imprimindo o valor da string "Pernambuco"
     printf("--- Card 1 ---\n");
-    printf("Código: %s\n", codigo);
-    printf("Estado: %s\n", estado);
-    printf("Capital: %s\n", capital);
-    printf("População: %d habitantes\n", populacao);
-    printf("Área: %.1f km²\n", area); // Use %.1f para formatar float com 1 casa decimal
-    printf("PIB: R$ %.2f  (Bilhões)\n", pib); // Use %.2f para formatar float com 2 casas decimais
-    printf("Pontos Turísticos: %d\n", turismo);
+    printf("Código: %s\n", p_codigo);
+    printf("Estado: %s\n", p_estado);
+    printf("Capital: %s\n", p_capital);
+    printf("População: %d habitantes\n", p_populacao);
+    printf("Área: %.1f km²\n", p_area); // Use %.1f para formatar float com 1 casa decimal
+    printf("PIB: R$ %.2f  (Bilhões)\n", p_pib); // Use %.2f para formatar float com 2 casas decimais
+    printf("Pontos Turísticos: %d\n", p_turismo);
+    // Novos atributos - FORMATADO COM DUAS CASAS DECIMAIS (%.2f)
+    printf("Densidade Populacional: %.2f hab/km²\n", p_densidade_pop);
+    printf("PIB per Capita: %.2f reais\n", p_pib_per_capita);
     printf("---------------------------\n");
 
     // Imprimindo o valor da string "Alagoas"
@@ -39,6 +55,9 @@ int main() {
     printf("Área: %.1f km²\n", a_area); // Use %.1f para formatar float com 1 casa decimal
     printf("PIB: R$ %.2f  (Bilhões)\n", a_pib); // Use %.2f para formatar float com 2 casas decimais
     printf("Pontos Turísticos: %d\n", a_turismo);
+    // Novos atributos - FORMATADO COM DUAS CASAS DECIMAIS (%.2f)
+    printf("Densidade Populacional: %.2f hab/km²\n", a_densidade_pop);
+    printf("PIB per Capita: %.2f reais\n", a_pib_per_capita);
     printf("---------------------------\n");
 
 
